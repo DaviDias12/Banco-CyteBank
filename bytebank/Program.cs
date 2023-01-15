03b3712e462be8e6c6b82d1872d11d1e384ca8b5
Davi Santos <davirr1220@gmail.com>
2023-01-15 15:36:32 -0300

    first commit

---
+++ bytebank/Program.cs
@@ -0,0 +1,9 @@
+﻿using bytebank;
+
+ContaCorrente contaDoAndre = new ContaCorrente();
+contaDoAndre.titular = "Andre Silva";
+contaDoAndre.numero_agencia = 15;
+contaDoAndre.conta = "1010-X";
+contaDoAndre.saldo = 100;
+
+Console.WriteLine("Saldo da conta do Andre = " + contaDoAndre.saldo);
